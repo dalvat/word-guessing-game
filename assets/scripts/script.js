7403b4180a531df1308af111d4a6a55bc85cb9eb
unknown <dmlswebdev@gmail.com>
2023-01-20 00:21:37 +0200

    javascript written for start game, random word and check

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,4 @@
-let wordsForGuessing = [
+let words = [
   "Child",
   "Think",
   "Thing",
@@ -42,3 +42,69 @@ let wordsForGuessing = [
   "Slate"
 ];
 
+let timeLeft = document.getElementById("timerLeft");
+let letter1 = document.getElementById("letter1");
+let letter2 = document.getElementById("letter2");
+let letter3 = document.getElementById("letter3");
+let letter4 = document.getElementById("letter4");
+let letter5 = document.getElementById("letter5");
+let startButton = document.getElementById("start");
+let resetButton = document.getElementById("reset");
+let wins = document.getElementById("winsNo");
+let loses = document.getElementById("losesNo");
+
+startButton.addEventListener("click", function(event) {
+  playGame();
+});
+
+let getRandom = function(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+let playGame = function() {
+randomWord();
+checkLetters();};
+
+let randomWord = function(getWord, chosenWord, wordSplit) {
+getWord = words[getRandom(0, words.length)];
+chosenWord = getWord.toUpperCase()
+wordSplit = chosenWord.split("");
+return wordSplit
+};
+
+let chosenWord = randomWord();
+
+let checkLetters = function(l1, l2, l3, l4, l5) {
+  l1 = chosenWord[0];
+  l2 = chosenWord[1];
+  l3 = chosenWord[2];
+  l4 = chosenWord[3];
+  l5 = chosenWord[4];
+
+  console.log(l1);
+  console.log(l2);
+  console.log(l3);
+  console.log(l4);
+  console.log(l5);
+  
+  addEventListener("keydown", function(event, typed, check) {
+    typed = event.key;
+    check = typed.toUpperCase();
+    switch(check) {
+      case l1:
+        letter1.textContent = l1;
+        break;
+      case l2:
+        letter2.textContent = l2;
+        break;
+      case l3:
+        letter3.textContent = l3;
+        break;
+      case l4:
+        letter4.textContent = l4;
+        break;
+      case l5:
+        letter5.textContent = l5;
+    }
+  });
+};
